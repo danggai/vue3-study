f82507d22e00eacfdb8955dd0763cd4c6c1194b5
danggai <donggi9313@naver.com>
2023-10-13 10:30:31 +0900

    vue3 프로젝트 구성 요소 및 컴포넌트 계층에 따른 학습

---
+++ vue3-cli/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <AppHeader 
+    :appTitle="msg" 
+    v-on:change="changeTitle">
+  </AppHeader>
+  <button @click="showAlert">경고</button>
+</div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  components: {
+    'AppHeader': AppHeader,
+  },
+  data() {
+    return {
+      msg: "앱 헤더 컴포넌트"
+    }
+  },
+  methods: {
+    showAlert() {
+      alert("hello")
+    },
+    changeTitle() {
+      this.msg = "새로운 " + this.msg
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue3-cli/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1> {{ appTitle }} </h1>
+        <button @click="changeTitle"> 바꿔잇 </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [
+            'appTitle'
+        ],
+        methods: {
+            changeTitle() {
+                this.$emit('change')
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
